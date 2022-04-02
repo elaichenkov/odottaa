467ecc9c8f0d4f28070a28ecf75655a09fca3618
Yevhen Laichenkov <elaichenkov@gmail.com>
2022-04-02 22:31:56 +0300

    tests: verify new matchers

--- tests/matchers.test.ts
+++ tests/matchers.test.ts
@@ -1,6 +1,23 @@
 import { test, expect } from '@playwright/test';
 
+const statusMap = new Map([
+  [201, 'toBeCreated'],
+  [401, 'toBeUnauthorized'],
+  [403, 'toBeForbidden'],
+  [404, 'toBeNotFound'],
+]);
+
 test.describe('matchers', () => {
+  statusMap.forEach((method, status) => {
+    test(`verify ${method}`, async ({ request }) => {
+      const response = await request.get(`${status}`);
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      await expect(response)[method]();
+    });
+  });
+
   test('verify toHaveStatusCode', async ({ request }) => {
     const response = await request.get('/404');
 
@@ -18,7 +35,7 @@ test.describe('matchers', () => {
   test('verify toHaveStatusText', async ({ request }) => {
     const response = await request.get('/404');
 
-    await expect(response).toHaveStatusText('Not found');
+    await expect(response).toHaveStatusText('Not Found');
   });
 
   test('verify toHaveJSON array of objects', async ({ request }) => {
@@ -28,6 +45,25 @@ test.describe('matchers', () => {
       { age: 30, name: 'John' },
       { age: 19, name: 'Adam' },
     ]);
+  });
+
+  test('verify toMatchJSON object', async ({ request }) => {
+    const response = await request.get('/data/object');
+
+    await expect(response).toMatchJSON({ age: 30, name: 'John' });
+  });
+
+  test('verify toMatchJSON nested object', async ({ request }) => {
+    const desiredHouse = {
+      bath: true,
+      kitchen: {
+        amenities: ['oven', 'stove', 'washer'],
+        wallColor: expect.stringMatching(/white|yellow/),
+      },
+    };
+    const response = await request.get('/data/object/nested');
+
+    await expect(response).toMatchJSON(desiredHouse);
   });
 
   test('verify toHaveJSON empty array', async ({ request }) => {
@@ -42,13 +78,6 @@ test.describe('matchers', () => {
     await expect(response).toHaveJSON({ age: 30, name: 'John' });
   });
 
-  test('fail toHaveJSON null', async ({ request }) => {
-    test.fail();
-    const response = await request.get('/da');
-
-    await expect(response).toHaveJSON(null);
-  });
-
   test('verify toContainJSON object', async ({ request }) => {
     const response = await request.get('/data/array');
 
@@ -58,17 +87,37 @@ test.describe('matchers', () => {
   test('verify toHaveText exact', async ({ request }) => {
     const response = await request.get('/');
 
-    await expect(response).toHaveText('<h1>Hello, World!</h1>');
+    await expect(response).toContainTextContent('<h1>Hello, World!</h1>');
   });
 
   test('verify toHaveText contain', async ({ request }) => {
     const response = await request.get('/');
 
-    await expect(response).toHaveText('Hello');
+    await expect(response).toContainTextContent('Hello');
   });
 
   test('verify toHaveHeader', async ({ request }) => {
     const response = await request.get('/');
-    await expect(response).toHaveHeader({ name: 'Content-Length', value: '22' });
+    await expect(response).toHaveHeader({ 'content-length': '22' });
+  });
+
+  test('verify toHaveHeaderName', async ({ request }) => {
+    const response = await request.get('/');
+    await expect(response).toHaveHeaderName('content-length');
+  });
+
+  test('verify toHaveContentType', async ({ request }) => {
+    const response = await request.get('/');
+    await expect(response).toHaveContentType('text/html');
+  });
+
+  test('verify toHaveLocation', async ({ request, baseURL }) => {
+    const response = await request.get('/');
+    await expect(response).toHaveLocation(baseURL!);
+  });
+
+  test('verify toBeRedirected', async ({ request, baseURL }) => {
+    const response = await request.get('/redirect');
+    await expect(response).toBeRedirected(`${baseURL}demo`);
   });
 });
